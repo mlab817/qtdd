dbe043bec32711154839c70d7a346193e95f1ab9
mlab817 <mlab817@gmail.com>
2021-01-07 22:19:29 +0800

    done search and projects index

--- src/pages/ProjectsIndex.vue
+++ src/pages/ProjectsIndex.vue
@@ -1,71 +1,70 @@
 <template>
   <q-page>
-    <template v-if="loading">
-      <div class="q-pa-md">Loading...</div>
-    </template>
-    <template v-else>
-      <q-list>
-        <div class="row q-pa-md items-center">
-          <div class="text-h5 text-weight-bolder text-primary">
-            Projects
+    <q-list>
+      <div class="row q-pa-md items-center">
+        <div class="text-h5 text-weight-bolder text-primary">
+          Projects
+        </div>
+        <q-space />
+        <!-- TODO: Hide this button if user has no permission to create project -->
+        <q-btn color="secondary" label="New Project" no-caps unelevated to="/projects/create" />
+      </div>
+      <div class="row q-pa-md">
+        <search-projects v-model="query" @input="searchProjects" />
+      </div>
+      <template v-if="loading">
+        <div class="row items-center justify-center q-pa-md">
+          <q-spinner color="primary"></q-spinner> Loading...
+        </div>
+      </template>
+      <template v-else>
+        <template v-if="projects.length > 0">
+          <q-item v-for="project in projects" :key="project.id" :clickable="project.permissions.view" @click.stop="viewProject(project.slug)">
+            <q-item-section avatar>
+              <q-avatar class="bg-primary text-white">
+                {{project.title && project.title.charAt(0)}}
+              </q-avatar>
+            </q-item-section>
+            <q-item-section>
+              <q-item-label class="text-uppercase">{{ project.title }}</q-item-label>
+              <q-item-label caption :lines="2">{{ project.description }}</q-item-label>
+            </q-item-section>
+            <q-item-section side>
+              <q-icon name="bookmark" color="grey-5" />
+            </q-item-section>
+          </q-item>
+          <div class="row justify-center q-my-md">
+            <q-pagination
+              v-model="currentPage"
+              :max-pages="10"
+              :max="lastPage"
+              :direction-links="true"
+              @input="navigatePage"
+            >
+            </q-pagination>
           </div>
-          <q-space />
-          <!-- TODO: Hide this button if user has no permission to create project -->
-          <q-btn color="secondary" label="New Project" no-caps unelevated to="/projects/create" />
+        </template>
+        <div class="row q-pa-md justify-center" v-else>
+          <template v-if="query">
+            No search results for "<span class="text-weight-bold">{{ query }}</span>".
+          </template>
+          <template v-else>
+            No projects found.
+          </template>
         </div>
-        <div class="row q-pa-md">
-          <q-input
-            outlined
-            v-model="query"
-            class="col"
-            placeholder="Search for projects..."
-            @keyup.enter="searchProjects"
-            @input="searchProjects"
-            debounce="500"
-            hint="Press Enter to search">
-            <template v-slot:append>
-              <q-icon name="send" @click="searchProjects" class="cursor-pointer" color="primary" />
-            </template>
-          </q-input>
-        </div>
-        <q-item v-for="project in projects" :key="project.id" :clickable="project.permissions.view" @click.stop="viewProject(project.slug)">
-          <q-item-section avatar>
-            <q-avatar class="bg-primary text-white">
-              {{project.title && project.title.charAt(0)}}
-            </q-avatar>
-          </q-item-section>
-          <q-item-section>
-            <q-item-label class="text-uppercase">{{ project.title }}</q-item-label>
-            <q-item-label caption :lines="2">{{ project.description }}</q-item-label>
-          </q-item-section>
-          <q-item-section side>
-          </q-item-section>
-        </q-item>
-      </q-list>
-
-      <div class="row justify-center q-my-md" v-if="projects.length > 0">
-        <q-pagination
-          v-model="currentPage"
-          :max-pages="10"
-          :max="lastPage"
-          :direction-links="true"
-          @input="navigatePage"
-        >
-        </q-pagination>
-      </div>
-      <div class="row q-pa-md justify-center" v-else>
-        No search results for "<span class="text-weight-bold">{{ query }}</span>".
-      </div>
-    </template>
+      </template>
+    </q-list>
 
   </q-page>
 </template>
 
 <script>
+import SearchProjects from 'components/SearchProjects'
 import { ProjectAPI } from '../api/projects'
 
 export default {
   name: 'ProjectsIndex',
+  components: { SearchProjects },
   data () {
     return {
       projects: [],
@@ -77,20 +76,13 @@ export default {
     }
   },
   watch: {
-    $route: function(to, from) {
-      const query = to.query
-      console.log(query)
+    $route: function (to) {
+      const params = to.query
+
+      this.getProjects(params)
     }
   },
   methods: {
-    searchProjects() {
-      this.$router.push({
-        name: 'index-projects',
-        query: {
-          query: this.query
-        }
-      })
-    },
     viewProject(slug) {
       this.$router.push(`/projects/${slug}`)
     },
@@ -106,20 +98,29 @@ export default {
       })
     },
     navigatePage(e) {
-      const query = {}
-      query.page = this.currentPage
-      query.query = this.query ? this.query : undefined
+      const query = this.$route.query
       this.$router.push({
         name: 'index-projects',
-        query: query
+        query: {
+          ...query,
+          page: e
+        }
       })
     },
-    getProjects() {
-      const page = this.currentPage ? this.currentPage : 1,
-        query = this.query ? this.query : null,
-        limit = this.limit ? this.limit : 10
+    searchProjects() {
+      const page = 1 // reset page to 1
+      this.$router.push({
+        name: 'index-projects',
+        query: {
+          query: this.query,
+          page: page
+        }
+      })
+    },
+    getProjects(params) {
+      this.loading = true
       ProjectAPI
-        .index({ params: { page: page, query: query, limit: limit } })
+        .index({ params: params })
         .then(res => {
           console.log(res)
           const {
@@ -135,13 +136,7 @@ export default {
     }
   },
   created() {
-    console.log(this.$route)
-    this.page = this.$route.query.page ? this.$route.query.page : 1
-    this.limit = this.$route.query.limit ? this.$route.query.limit : 10
-    this.query = this.$route.query.query ? this.$route.query.query : null
-    this.getProjects()
-    ProjectAPI.index()
-      .then(res => console.log(res))
+    this.getProjects(null)
   }
 }
 </script>
