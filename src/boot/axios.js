e4814a332816232811173de91d14935102a1150d
Mark Lester Bolotaolo <mlab817@gmail.com>
2021-01-13 16:05:28 +0800

    made https

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.DEV ? 'http://tdd.test/api/v1' : 'http://ipms-v2.dapmsipd.org/api/v1'
+  baseURL: process.env.DEV
+    ? 'http://tdd.test/api/v1'
+    : 'https://ipms-v2.dapmsipd.org/api/v1'
 })
 
 Vue.prototype.$axios = axiosInstance
