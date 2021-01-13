7b42c02a5b887f0c22870f06593e34b5b486bd55
Mark Lester Bolotaolo <mlab817@gmail.com>
2021-01-13 15:45:06 +0800

    changed url

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://tdd.test/api/v1'
+  baseURL: process.env.DEV ? 'http://tdd.test/api/v1' : 'http://ipms-v2.dapmsipd.org/api/v1'
 })
 
 Vue.prototype.$axios = axiosInstance
