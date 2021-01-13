ab5b7836006b2de50eac4945e04305ef8f855520
Mark Lester Bolotaolo <mlab817@gmail.com>
2021-01-13 16:14:07 +0800

    add link to backend

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,6 +2,18 @@
   <q-page padding>
     <div class="row q-col-gutter-md">
       <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-xs-12">
+        <div class="q-py-md">
+          <q-banner class="bg-primary text-white">
+            <template v-slot:avatar>
+              <q-icon name="warning"></q-icon>
+            </template>
+            Note: If the data is not loading, please click in the link below to allow access to the backend.
+            <template v-slot:action>
+              <q-btn type="a" href="https://ipms-v2.dapmsipd.org/" label="Go" flat></q-btn>
+            </template>
+          </q-banner>
+        </div>
+
         <bar-chart />
       </div>
       <div class="col"></div>
