ac5971edc1a67528491de567a0a81e772b792ef0
Mark Lester Bolotaolo <mlab817@gmail.com>
2021-01-28 09:58:20 +0800

    added loading indicators

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -17,13 +17,13 @@
       </div>
     </div>
     <div class="row">
-      <e-charts style="height: 300px;" :options="barOptions"></e-charts>
-      <e-charts style="height: 300px;" :options="barOptions2"></e-charts>
-      <e-charts style="height: 300px;" :options="barOptions3"></e-charts>
-      <e-charts style="height: 300px;" :options="barOptions4"></e-charts>
-      <e-charts style="height: 300px;" :options="barOptions5"></e-charts>
-      <e-charts style="height: 300px;" :options="barOptions6"></e-charts>
-      <e-charts style="height: 300px;" :options="barOptions7"></e-charts>
+      <e-charts style="height: 300px;" :options="barOptions" ref="bar1"></e-charts>
+      <e-charts style="height: 300px;" :options="barOptions2" ref="bar2"></e-charts>
+      <e-charts style="height: 300px;" :options="barOptions3" ref="bar3"></e-charts>
+      <e-charts style="height: 300px;" :options="barOptions4" ref="bar4"></e-charts>
+      <e-charts style="height: 300px;" :options="barOptions5" ref="bar5"></e-charts>
+      <e-charts style="height: 300px;" :options="barOptions6" ref="bar6"></e-charts>
+      <e-charts style="height: 300px;" :options="barOptions7" ref="bar7"></e-charts>
     </div>
   </q-page>
 </template>
@@ -241,42 +241,62 @@ export default {
     }
   },
   mounted() {
+    const bar1 = this.$refs.bar1,
+      bar2 = this.$refs.bar2,
+      bar3 = this.$refs.bar3,
+      bar4 = this.$refs.bar4,
+      bar5 = this.$refs.bar5,
+      bar6 = this.$refs.bar6,
+      bar7 = this.$refs.bar7
+    bar1.showLoading()
+    bar2.showLoading()
+    bar3.showLoading()
+    bar4.showLoading()
+    bar5.showLoading()
+    bar6.showLoading()
+    bar7.showLoading()
     this.$axios.get('/chart/spatial_coverages')
       .then(res => {
         this.barOptions.title.text = res.data.title
         this.barOptions.xAxis.data = res.data.categories
         this.barOptions.series[0].data = res.data.data
       })
+      .finally(() => bar1.hideLoading())
     this.$axios.get('/chart/pap_types')
       .then(res => {
         this.barOptions2.title.text = res.data.title
         this.barOptions2.xAxis.data = res.data.categories
         this.barOptions2.series[0].data = res.data.data
       })
+      .finally(() => bar2.hideLoading())
     this.$axios.get('/chart/regions')
       .then(res => {
         this.barOptions3.title.text = res.data.title
         this.barOptions3.xAxis.data = res.data.categories
         this.barOptions3.series[0].data = res.data.data
       })
+      .finally(() => bar3.hideLoading())
     this.$axios.get('/chart/implementation_start')
       .then(res => {
         this.barOptions4.title.text = res.data.title
         this.barOptions4.xAxis.data = res.data.categories
         this.barOptions4.series[0].data = res.data.data
       })
+      .finally(() => bar4.hideLoading())
     this.$axios.get('/chart/main_funding_source')
       .then(res => {
         this.barOptions5.title.text = res.data.title
         this.barOptions5.xAxis.data = res.data.categories
         this.barOptions5.series[0].data = res.data.data
       })
+      .finally(() => bar5.hideLoading())
     this.$axios.get('/chart/office')
       .then(res => {
         this.barOptions6.title.text = res.data.title
         this.barOptions6.xAxis.data = res.data.categories
         this.barOptions6.series[0].data = res.data.data
       })
+      .finally(() => bar6.hideLoading())
     this.$axios.get('/chart/pip')
       .then(res => {
         this.barOptions7.title.text = res.data.title
@@ -290,6 +310,7 @@ export default {
         })
         this.barOptions7.series[0].data = data
       })
+      .finally(() => bar7.hideLoading())
   }
 }
 </script>
